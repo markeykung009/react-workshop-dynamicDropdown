dafc3ad8c1407a146ce8ef8495e6395b190cd6ef
markeykung009 <markeykung009@gmail.com>
2023-05-20 01:41:39 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import DropdownComponent from "./components/DropdownComponent";
+import FoodComponent from "./components/FoodComponent";
+import MenuData from "./data/MenuData";
 
 function App() {
+  const [food, setFood] = useState(MenuData);
+
+  const changeFoodData = e => {
+    const category = e.target.value;
+    if (category === "เมนูทั้งหมด") {
+      setFood(MenuData);
+    } else {
+      const result = MenuData.filter(el => {
+        return el.menu === category;
+      });
+      setFood(result);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <DropdownComponent changeFoodData={changeFoodData} />
+      <div className="content">
+        {food.map((el, idx) => (
+          <FoodComponent key={idx} {...el} />
+        ))}
+      </div>
     </div>
   );
 }
